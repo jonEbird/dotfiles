43f2b74c2eedd65917187f80af1f8b89d5576194
Jon Miller <jonEbird@gmail.com>
2012-05-08 11:12:44 -0400

    Updated Windows emacs startup BAT script for new Win7 build

--- bin/runemacs.bat
+++ bin/runemacs.bat
@@ -1,18 +1,16 @@
-set "HOME=C:\Documents and Settings\millej95\Soft\emacs24\config"
+set BASE=C:\Users\millej95
+set EMACSVERSION=24.1.50
+REM
+REM
+set "HOME=%BASE%\soft\config"
+cd %HOME%
 REM Note, we actually start in the HOME directory via the shortcut
 del /P "*.lock"
-set PATH=%PATH%;C:\Program Files\Git\bin\
-set PATH=%PATH%;C:\Documents and Settings\millej95\Soft\emacs24\Emacs\bin\
-set PATH=%PATH%;C:\Documents and Settings\millej95\Soft\miktex\bin\
-set PATH=%PATH%;C:\Documents and Settings\millej95\My Documents\Downloads\
-set PATH=%PATH%;C:\Documents and Settings\millej95\Soft\utility\bin\
+set PATH=%PATH%;"C:\Program Files (x86)\Git\bin\"
+set PATH=%PATH%;%BASE%\soft\emacs-%EMACSVERSION%\bin\
+set PATH=%PATH%;%BASE%\Downloads\
 REM
 REM pageant.exe "C:\Documents and Settings\millej95\soft\putty_keys\private.ppk"
 REM runemacs.exe
 runemacs.exe -mm
 REM pause
-REM
-REM
-REM set HOME=%CD%\config
-REM %CD%\ntemacs23\bin\runemacs.exe
-REM set PATH=C:\Program Files\Git\bin\;%PATH%
