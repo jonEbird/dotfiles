e613a13b318ad2497fac544d323f30881f5e8d8a
Jon Miller <jonEbird@gmail.com>
2012-09-30 18:02:52 -0400

    Finally adding remote editing as root via sudo to my Tramp config

--- .emacs.d/my_configs/tramp_config.el
+++ .emacs.d/my_configs/tramp_config.el
@@ -2,6 +2,11 @@
 
 (setq tramp-remote-path (quote (tramp-default-remote-path "/usr/sbin" "/usr/local/bin" "/local/bin" "/local/freeware/bin" "/local/gnu/bin" "/usr/freeware/bin" "/usr/pkg/bin" "/usr/contrib/bin" "/usr/bin")))
 
+(add-to-list 'tramp-default-proxies-alist
+             '(nil "\\`root\\'" "/ssh:%h:"))
+(add-to-list 'tramp-default-proxies-alist
+             '((regexp-quote (system-name)) nil nil))
+
 (if (eq system-type 'windows-nt)
     (setq tramp-default-method "pscp")
   )
